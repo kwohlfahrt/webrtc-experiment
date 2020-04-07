755cdcfefcadae1f1ff3013b496c386eb52087cb
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2020-04-07 13:49:53 +0100

    First self-to-self client

---
+++ client/src/index.ts
@@ -0,0 +1,33 @@
+async function main() {
+  const localVideo = document.getElementById("localVideo")! as any as HTMLVideoElement;
+  const remoteVideo = document.getElementById("remoteVideo")! as any as HTMLVideoElement;
+
+  const media = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  localVideo.srcObject = media;
+
+  const connections = [new RTCPeerConnection(), new RTCPeerConnection()];
+  media.getTracks().forEach(track => connections[0].addTrack(track, media));
+
+  connections[0].addEventListener("icecandidate", e =>
+    e.candidate && connections[1].addIceCandidate(e.candidate),
+  );
+  connections[1].addEventListener("icecandidate", e =>
+    e.candidate && connections[0].addIceCandidate(e.candidate),
+  );
+  connections[1].addEventListener("track", e => {
+    e.streams.forEach(stream => (remoteVideo.srcObject = stream));
+  });
+
+  const offer = await connections[0].createOffer();
+  await connections[0].setLocalDescription(offer);
+  await connections[1].setRemoteDescription(offer);
+
+  const answer = await connections[1].createAnswer();
+  await connections[1].setLocalDescription(answer);
+  await connections[0].setRemoteDescription(answer);
+}
+
+main();
